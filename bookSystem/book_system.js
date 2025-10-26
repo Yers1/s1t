b7161028cb50cebb6cc9c9099aa1688e5c6baccb
Yers1 <ersultanmukatay13@gmail.com>
2025-10-26 11:11:43 -0400

    Book Management System, with extra delete function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -21,15 +21,18 @@ function addBook() {
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+    const booksDiv = books.map((book, index) => `
+        <h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
-    );
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>
+    `);
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
+
 
 
 function editbook(index) {
@@ -49,3 +52,10 @@ function clearInputs() {
     document.getElementById('pagesNumber').value = '';
 }
 
+function deletebook(index) {
+   
+    books.splice(index, 1);
+
+   
+    showbooks();
+}
